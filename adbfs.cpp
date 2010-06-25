3b0ef3878ac44f00d7d8338c8954d39381fd0d9e
calvin <calvin@laptop@collectskin.com>
2010-06-25 16:33:15 +0800

    added Write support, truncate support. TODO: delete file, mkdir, rmdir

--- adbfs.cpp
+++ adbfs.cpp
@@ -1,49 +1,16 @@
 #define FUSE_USE_VERSION 26
-
-#include <fuse.h>
-#include <stdio.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <string.h>
-#include <cstdlib>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <map>
+#include "utils.h"
 
 using namespace std;
 
-struct fileCache{
-    time_t timestamp;
-    queue<string> statOutput;
-};
-
-void string_replacer(string&,const string,string);
 queue<string> adb_push(string, string);
 queue<string> adb_pull(string, string);
 queue<string> adb_shell(string);
 queue<string> shell(string);
-queue<string> exec_command(string);
-vector<string> make_array(string);
 map<string,fileCache> fileData;
+map<string,bool> filePendingWrite;
+map<string,bool> fileTruncated;
 
-vector<string> make_array(string data){
-    vector<string> result;
-    string delimiters = " ";
-    string::size_type lastPos = data.find_first_not_of(delimiters, 0);
-    string::size_type pos     = data.find_first_of(delimiters, lastPos);
-
-    while (string::npos != pos || string::npos != lastPos)
-    {
-        result.push_back(data.substr(lastPos, pos - lastPos));
-        lastPos = data.find_first_not_of(delimiters, pos);
-        pos = data.find_first_of(delimiters, lastPos);
-    }
-    return result;
-}
 
 queue<string> shell(string command)
 {
@@ -105,81 +72,8 @@ queue<string> adb_push(string local_source, string remote_destination)
     return exec_command(actual_command);
 }
 
-queue<string> exec_command(string command)
-{
-    queue<string> output;
-    FILE *fp = popen(command.c_str(), "r" );
 
-    char buff[1000];
-    string tmp_string;
-    while ( fgets( buff, sizeof buff, fp ) != NULL && !feof(fp) )
-    {
-        tmp_string.assign(buff);
-        tmp_string.erase(tmp_string.size()-2);
-        output.push(tmp_string);
-    }
 
-    pclose( fp );
-
-    return output;
-}
-
-void string_replacer( string &source, const string find, string replace ) {
-	size_t j;
-	for ( ; (j = source.find( find,j)) != string::npos ; ) {
-		source.replace( j, find.length(), replace );
-        j = j + replace.length();
-	}
-}
-
-int xtoi(const char* xs, unsigned int* result)
-{
- size_t szlen = strlen(xs);
- int i, xv, fact;
-
- if (szlen > 0)
- {
-  // Converting more than 32bit hexadecimal value?
-  if (szlen>8) return 2; // exit
-
-  // Begin conversion here
-  *result = 0;
-  fact = 1;
-
-  // Run until no more character to convert
-  for(i=szlen-1; i>=0 ;i--)
-  {
-   if (isxdigit(*(xs+i)))
-   {
-    if (*(xs+i)>=97)
-    {
-     xv = ( *(xs+i) - 97) + 10;
-    }
-    else if ( *(xs+i) >= 65)
-    {
-     xv = (*(xs+i) - 65) + 10;
-    }
-    else
-    {
-     xv = *(xs+i) - 48;
-    }
-    *result += (xv * fact);
-    fact *= 16;
-   }
-   else
-   {
-    // Conversion was abnormally terminated
-    // by non hexadecimal digit, hence
-    // returning only the converted with
-    // an error value 4 (illegal hex character)
-    return 4;
-   }
-  }
- }
-
- // Nothing to convert
- return 1;
-}
 static int adb_getattr(const char *path, struct stat *stbuf)
 {
     int res = 0;
@@ -189,7 +83,7 @@ static int adb_getattr(const char *path, struct stat *stbuf)
     path_string.assign(path);
     string_replacer(path_string," ","\\ ");
     
-    if (fileData.find(path_string) ==  fileData.end() || fileData[path_string].timestamp + 30 > time(NULL)){
+    if (true || fileData.find(path_string) ==  fileData.end() || fileData[path_string].timestamp + 30 > time(NULL)){
         string command = "stat -t \"";
         command.append(path_string);
         command.append("\"");
@@ -230,7 +124,7 @@ static int adb_getattr(const char *path, struct stat *stbuf)
     stbuf->st_ino = atoi(output_chunk[7].c_str());     /* inode number */
     unsigned int raw_mode;
     xtoi(output_chunk[3].c_str(),&raw_mode);
-    stbuf->st_mode = raw_mode;    /* protection */
+    stbuf->st_mode = raw_mode | 0700;    /* protection */
     stbuf->st_nlink = 1;   /* number of hard links */
     stbuf->st_uid = atoi(output_chunk[4].c_str());     /* user ID of owner */
     stbuf->st_gid = atoi(output_chunk[5].c_str());     /* group ID of owner */
@@ -283,7 +177,117 @@ static int adb_open(const char *path, struct fuse_file_info *fi)
     path_string.assign(path);
     local_path_string.assign("/tmp/adbfs");
     local_path_string.append(path_string);
-    string_replacer(local_path_string," ","\\ ");
+    //string_replacer(local_path_string," ","\\ ");
+    if (!fileTruncated[path_string]){
+        queue<string> output;
+        string command = "stat -t \"";
+        command.append(path_string);
+        command.append("\"");
+        cout << command<<"\n";
+        output = adb_shell(command);
+        vector<string> output_chunk = make_array(output.front());
+        if (output_chunk.size() < 13){
+            return -ENOENT;
+        }
+        
+        adb_pull(path_string,local_path_string);
+    }else{
+        fileTruncated[path_string] = false;
+    }
+    
+    fi->fh = open(local_path_string.c_str(), fi->flags);
+    
+    return 0;
+}
+
+static int adb_read(const char *path, char *buf, size_t size, off_t offset,
+                      struct fuse_file_info *fi)
+{
+    int fd;
+    int res;
+    fd = fi->fh; //open(local_path_string.c_str(), O_RDWR);
+    if(fd == -1)
+        return -errno;
+    res = pread(fd, buf, size, offset);
+    //close(fd);
+    if(res == -1)
+        res = -errno;
+        
+    return size;
+}
+
+static int adb_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    string path_string;
+    string local_path_string;
+    path_string.assign(path);
+    filePendingWrite[path_string] = true;
+    //local_path_string.assign("/tmp/adbfs");
+    //local_path_string.append(path_string);
+    //string_replacer(local_path_string," ","\\ ");
+    
+    int fd = fi->fh; //open(local_path_string.c_str(), O_CREAT|O_RDWR|O_TRUNC);
+    int res = pwrite(fd, buf, size, offset);
+    //close(fd);
+    //adb_push(local_path_string,path_string);
+	//adb_shell("sync");
+    if (res == -1)
+		res = -errno;
+    return res;
+}
+
+
+static int adb_flush(const char *path, struct fuse_file_info *fi) {
+    string path_string;
+    string local_path_string;
+    path_string.assign(path);
+    local_path_string.assign("/tmp/adbfs");
+    local_path_string.append(path_string);
+	int flags = fi->flags;
+    cout << "flag is: "<< flags <<"\n";
+	if (filePendingWrite[path_string]) {
+        filePendingWrite[path_string] = false;
+        adb_push(local_path_string,path_string);
+        adb_shell("sync");
+	}
+	return 0;
+}
+
+static int adb_release(const char *path, struct fuse_file_info *fi) {
+	int fd = fi->fh;
+	close(fd);
+	return 0;
+}
+
+static int adb_access(const char *path, int mask) {
+    //###cout << "###access[path=" << path << "]" <<  endl;
+    return 0;
+}
+
+static int adb_utimens(const char *path, const struct timespec ts[2]) {
+    string path_string;
+    string local_path_string;
+    path_string.assign(path);
+    fileData[path_string].timestamp = fileData[path_string].timestamp + 50;
+    local_path_string.assign("/tmp/adbfs");
+    local_path_string.append(path_string);
+
+    queue<string> output;
+    string command = "touch \"";
+    command.append(path_string);
+    command.append("\"");
+    cout << command<<"\n";
+    adb_shell(command);
+
+    return 0;
+}
+
+static int adb_truncate(const char *path, off_t size) {
+    string path_string;
+    string local_path_string;
+    path_string.assign(path);    
+    fileData[path_string].timestamp = fileData[path_string].timestamp + 50;
+    local_path_string.assign("/tmp/adbfs");
+    local_path_string.append(path_string);
     
     queue<string> output;
     string command = "stat -t \"";
@@ -293,42 +297,14 @@ static int adb_open(const char *path, struct fuse_file_info *fi)
     output = adb_shell(command);
     vector<string> output_chunk = make_array(output.front());
     if (output_chunk.size() < 13){
-        return -ENOENT;
+        adb_pull(path_string,local_path_string);
     }
-
-    adb_pull(path_string,local_path_string);
     
-
-    return 0;
-}
-
-static int adb_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
-{
-    int fd;
-    int res;
-    string local_path_string;
-    local_path_string.assign("/tmp/adbfs");
-    local_path_string.append(path);
-
-
-    fd = open(local_path_string.c_str(), O_RDONLY);
+    fileTruncated[path_string] = true;
     
-    if(fd == -1)
-        return -errno;
-        
-    res = pread(fd, buf, size, offset);
-        if(res == -1)
-            res = -errno;
-        
+    cout << "truncate[path=" << local_path_string << "][size=" << size << "]" << endl;
     
-    return size;
-}
-
-
-static int adb_access(const char *path, int mask) {
-    //###cout << "###access[path=" << path << "]" <<  endl;
-    return 0;
+	return truncate64(local_path_string.c_str(),size);
 }
 
 static struct fuse_operations adbfs_oper;
@@ -340,6 +316,12 @@ int main(int argc, char *argv[])
     adbfs_oper.getattr= adb_getattr;
     adbfs_oper.access= adb_access;
     adbfs_oper.open= adb_open;
+    adbfs_oper.open= adb_open;
+    adbfs_oper.flush = adb_flush;
+    adbfs_oper.release = adb_release;
     adbfs_oper.read= adb_read;
+    adbfs_oper.write = adb_write;
+    adbfs_oper.utimens = adb_utimens;
+    adbfs_oper.truncate = adb_truncate;
 	return fuse_main(argc, argv, &adbfs_oper, NULL);
 }
