0b740ffd0a0644171f0bfc39186eb23dcf200c24
Calvin Tee <calvin@alphamelon.com>
2011-10-13 14:16:15 +0800

    update license

--- adbfs.cpp
+++ adbfs.cpp
@@ -1,4 +1,9 @@
 /*
+ *      Software License Agreement (BSD License)
+ *
+ *      Copyright (c) 2010-2011, Calvin Tee (collectskin.com)
+ *      All rights reserved.
+ *
  *      Redistribution and use in source and binary forms, with or without
  *      modification, are permitted provided that the following conditions are
  *      met:
@@ -25,6 +30,7 @@
  *      (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+ 
 #define FUSE_USE_VERSION 26
 #include "utils.h"
 
