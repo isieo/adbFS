218a85a213219be677db5591d0ee722d82584439
Konstantin Stepanov <me@kstep.me>
2013-02-16 01:03:46 +0300

    Use real readlink command for readlink implementation

--- adbfs.cpp
+++ adbfs.cpp
@@ -605,26 +605,25 @@ static int adb_unlink(const char *path) {
 static int adb_readlink(const char *path, char *buf, size_t size)
 {
     string path_string(path);
-    string_replacer(path_string,"'","\\'");
-    queue<string> output;
-    string command = "ls -l --color=none \"";
+    string_replacer(path_string, "'", "\\'");
+
+    string command = "readlink \"";
     command.append(path_string);
     command.append("\"");
+
+    queue<string> output;
     output = adb_shell(command);
+
     if(output.empty())
        return -EINVAL;
+
     string res = output.front();
-    size_t pos = res.find(" -> ");
-    if(pos == string::npos)
-       return -EINVAL;
-    pos+=4;
-    while(res[pos] == '/')
-       ++pos;
-    size_t my_size = res.size() - pos;
+
+    size_t my_size = res.size();
     if(my_size >= size)
        return -ENOSYS;
-    //cout << res << endl << pStart <<endl;
-    memcpy(buf, res.c_str() + pos, my_size+1);
+
+    memcpy(buf, res.c_str(), my_size);
     return 0;
 }
 
