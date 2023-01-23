39381d869b08ff5107df8dab04525459359d0ec9
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-01-23 00:38:52 +0000

    Initial successful test for Android 10 and fuse v1

--- docker/run-docker-test.sh
+++ docker/run-docker-test.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
 echo Running supervisord in the background
+# cd /root || exit
 /usr/bin/supervisord --configuration supervisord.conf &
+
+cd /src || exit
 
 
 echo Checking readiness via adb shell ls -d /
 
-RETRIES=30
+RETRIES=60
 
 while [ $RETRIES -gt 0 ];
 do
-  echo hi $RETRIES
   RETRIES=$((RETRIES - 1))
 
   OUTPUT=$(adb shell ls -d /)
 
-  echo "Output $OUTPUT"
-  echo "adb devices"
-  adb devices
-  sleep 3
+  if [ "$OUTPUT" == "/" ]
+  then
+    break;
+  fi
+  sleep 1
+  # adb connect 127.0.0.1:5555
 done
+
+if [ $RETRIES -le 0 ]
+then
+  echo "Could not connect to emulator, exiting"
+  exit 1
+fi
+
+echo Ready to run adbfs
+
+mkdir -p /adbfs
+./adbfs /adbfs
+
+ls -la /adbfs
