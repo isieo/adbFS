75d3d9a73e317b1e058a3966cbec21b26810ac40
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-01-24 17:06:02 +0000

    initial tests working

--- docker/run-docker-test.sh
+++ docker/run-docker-test.sh
@@ -7,33 +7,72 @@ echo Running supervisord in the background
 cd /src || exit
 
 
-echo Checking readiness via adb shell ls -d /
 
-RETRIES=60
+wait_available() {
 
-while [ $RETRIES -gt 0 ];
-do
-  RETRIES=$((RETRIES - 1))
+  RETRIES=60
+  WAIT_DIR="$1"
 
-  OUTPUT=$(adb shell ls -d /)
+  while [ $RETRIES -gt 0 ];
+  do
+    RETRIES=$((RETRIES - 1))
 
-  if [ "$OUTPUT" == "/" ]
+    OUTPUT=$(adb shell ls -d "$WAIT_DIR" 2> /dev/null)
+
+    if [ "$OUTPUT" == "$WAIT_DIR" ]
+    then
+      break;
+    fi
+    sleep 1
+
+  done
+
+  if [ $RETRIES -le 0 ]
   then
-    break;
+    echo "Emulator directory $1 was not available, exiting"
+    exit 1
   fi
-  sleep 1
-  # adb connect 127.0.0.1:5555
-done
 
-if [ $RETRIES -le 0 ]
-then
-  echo "Could not connect to emulator, exiting"
-  exit 1
-fi
+}
 
-echo Ready to run adbfs
+echo Checking readiness via adb shell ls -d /sdcard/Android
+wait_available /sdcard/Android
 
 mkdir -p /adbfs
 ./adbfs /adbfs
 
-ls -la /adbfs
+echo Ready to run adbfs tests
+
+BASE_DIR=/adbfs/sdcard/test
+
+test_mkdir() {
+
+  mkdir "$BASE_DIR/x"
+  output=$(ls -lad "$BASE_DIR/x")
+  # TODO: verify output is correct
+
+}
+
+
+
+mkdir "$BASE_DIR"
+
+test_mkdir
+
+
+
+
+# todo, test mkdir
+
+# create file with cat
+
+# copy file
+
+
+# touch to update time
+
+# copy preserving timestamps
+
+rm -rf "$BASE_DIR"
+
+
